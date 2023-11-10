771e0cc41f6efd890989598093bf38239576f808
ventusoon <sia@ik.me>
2023-11-10 12:26:14 +0000

    Deploying to gh-pages from @ ventusoon/LuvSia.com@1e3fb1a731b829f104ad80da3ff4bf59a715a383 🚀

---
+++ assets/pageview-7ac83a74.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./app-bc2ed064.js";const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-bc2ed064.js").then(r=>r.O),[]);return e({serverURL:"https://waline-comment.vuejs.press"})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
